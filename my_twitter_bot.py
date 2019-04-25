b0c039667bb88c5a56df213bf66945d5bf58933b
sneha <30837089+itmesneha@users.noreply.github.com>
2019-04-26 00:31:51 +0530

    Update my_twitter_bot.py

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -1,13 +1,10 @@
 import tweepy
 import time
 
-CONSUMER_KEY = 'eIf3G8nOjNjD4wxXTFTHcLPUx'
-CONSUMER_SECRET = 'hG92wsgDVp50YaXsVZ3dRQ38aLAniPn7WZtf1mkdtEDH6OrWkA'
-ACCESS_KEY = '722070452420501506-q5nyNzpHWYD0PVtrxZ847KN4l07CXRj'
-ACCESS_SECRET = '5TPgNLQIQGM0sX12CTAte81lMR2l5FyJobyc8gP3KGFRI'
-# NOTE: flush=True is just for running this script
-# with PythonAnywhere's always-on task.
-# More info: https://help.pythonanywhere.com/pages/AlwaysOnTasks/
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 print('this is my twitter bot')
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -30,11 +27,7 @@ def store_last_seen_id(last_seen_id, file_name):
 
 def reply_to_tweets():
     print('retrieving and replying to tweets...', flush=True)
-    # DEV NOTE: use 1060651988453654528 for testing.
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
-    # NOTE: We need to use tweet_mode='extended' below to show
-    # all full tweets (with full_text). Without it, long tweets
-    # would be cut off.
     mentions = api.mentions_timeline(
                         last_seen_id,
                         tweet_mode='extended')
